c8c95c4cbb4b6106ff76fcba418f9720f445487d
Darshan-gowda-m <155784473+Darshan-gowda-m@users.noreply.github.com>
2025-07-29 00:10:25 +0530

    Update 9.py

--- simple/9.py
+++ simple/9.py
@@ -1,29 +1,26 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
+np.random.seed(0)
 def lwr(x,y,x0,tau):
     m=len(x)
     X=np.c_[np.ones(m),x]
     X0=np.array([1,x0])
     w=np.exp(-((x-x0)**2)/(2*tau**2))
     W=np.diag(w)
-    thetha=np.linalg.pinv(X.T @ W @ X)@ X.T @ W @ y
-    return X0@ thetha
+    thetha=np.linalg.pinv(X.T@W@X)@ X.T@ W @ y
+    return X0@thetha
 
-
-np.random.seed(0)
 x=np.linspace(0,10,100)
 y=np.sin(x)+0.3*np.random.randn(100)
-
-x_test=np.linspace(0,10,500)
-y_test=np.array([lwr(x,y,x0,tau=0.3) for x0 in x_test])
-
+x_t=np.linspace(0,10,300)
+y_t=np.array([lwr(x,y,x0,tau=0.3) for x0 in x_t])
 
 plt.figure(figsize=(8,4))
-
-plt.plot(x_test,y_test,color='green')
-plt.scatter(x,y,color='yellow')
-plt.title('LWR')
+plt.title("LWR")
+plt.scatter(x,y,color='green')
+plt.plot(x_t,y_t,color='blue')
 plt.show()
